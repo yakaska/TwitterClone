6c5a923c2b63ed1bcc7586829801e1bf648801ad
Valentin <valentin7petr@gmail.com>
2022-06-27 21:29:04 +0300

    Initial commit

---
+++ src/test/java/org/yakaska/twitterclone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yakaska.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
